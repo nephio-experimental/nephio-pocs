d08fc9a0c0b138722a8c42516d438a959667ff16
John Belamaric <jbelamaric@google.com>
2022-11-08 19:25:16 -0800

    Bare initial skeleton of FiveGCoreTopology controller

--- nephio-5gc-controller/controllers/nf/fivegcoretopology_controller.go
+++ nephio-5gc-controller/controllers/nf/fivegcoretopology_controller.go
@@ -19,11 +19,15 @@ package nf
 import (
 	"context"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	"github.com/go-logr/logr"
+
+	autov1alpha1 "github.com/nephio-project/nephio-controller-poc/apis/automation/v1alpha1"
 	nfv1alpha1 "github.com/nephio-project/nephio-pocs/nephio-5gc-controller/apis/nf/v1alpha1"
 )
 
@@ -31,6 +35,8 @@ import (
 type FiveGCoreTopologyReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+
+	l logr.Logger
 }
 
 //+kubebuilder:rbac:groups=nf.nephio.org,resources=fivegcoretopologies,verbs=get;list;watch;create;update;patch;delete
@@ -47,11 +53,52 @@ type FiveGCoreTopologyReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
 func (r *FiveGCoreTopologyReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	r.l = log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	var topo nfv1alpha1.FiveGCoreTopology
+	if err := r.Get(ctx, req.NamespacedName, &topo); err != nil {
+		r.l.Error(err, "unable to fetch FiveGCoreTopology")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	r.l.Info("loaded", "topo", topo)
+
+	for _, u := range topo.Spec.UPFs {
+		// for each UPFClusterSet, create a PackageDeployment
+		// the reference package should have a UPFDeployment injection
+		// point, which we will later connect back here via the ownerRef and UPFClusterSet name
+		// TODO: add a validation that UPFClusterSet name is unique within a FiveGCoreTopology resource
+		//
+
+		// find the UPF class
+		upfClassName := u.UPF.UPFClassName
+		upfClass, err := r.getUPFClass(ctx, upfClassName)
+		if err != nil {
+			r.l.Error(err, "unable to fetch UPFClass %q", upfClassName)
+			return ctrl.Result{}, err
+		}
+
+		// create the PackageDeployment
+		pd := autov1alpha1.PackageDeployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      topo.Name + "-" + u.Name, //TODO: Fix this garbage
+				Namespace: topo.Namespace,
+			},
+			Spec: autov1alpha1.PackageDeploymentSpec{
+				Selector:   &u.Selector,
+				PackageRef: upfClass.Spec.PackageRef,
+				Namespace:  &u.Namespace,
+			},
+		}
+
+		r.l.Info("creating", "pd", pd)
+	}
 
 	return ctrl.Result{}, nil
+}
+
+func (r *FiveGCoreTopologyReconciler) getUPFClass(ctx context.Context, upfClassName string) (*nfv1alpha1.UPFClass, error) {
+	return &nfv1alpha1.UPFClass{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
